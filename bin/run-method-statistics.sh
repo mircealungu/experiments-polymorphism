cbe72dfac998446a893ecc895950b745fd7bf120
Mircea Lungu <lungu@iam.unibe.ch>
2012-04-19 11:50:03 +0200

    added script for computing method statistics

---
+++ bin/run-method-statistics.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo running method statistics...
+
+basedir=`cd .. && pwd`
+app=$basedir/bin/PolyExp.app/Contents/MacOS/visual 
+im=$basedir/bin/PolyExp.app/Contents/Resources/resource.im
+stimdir=$basedir/stimages
+
+metstatfile=$basedir/results/method-statistics.csv
+
+for imfile in `ls $basedir/stimages/*.im`
+do
+	system=`basename $imgile .im`
+
+	if cat $metstatfile | grep -q $system
+	then
+		echo $system is already analyzed
+	else
+		command="[|an| 
+				Undeclared class superclass methodDictionary 
+					at: #purgeUnusedBindings 
+					put: (Undeclared class superclass methodDictionary at: #asString).
+
+				Store.DbRegistry 
+					connectTo: ((Store.ConnectionProfile new) name: 'bern'; 
+					driverClassName: #PostgreSQLEXDIConnection; 
+					environment: 'db.iam.unibe.ch:5432_scgStore'; 
+					userName: 'storeguest'; 
+					password: 'storeguest'; 
+					tableOwner: 'BERN'; 
+					yourself). 
+				(Store.Bundle new name: 'Mircea-Experiments') mostRecentVersion loadSrc.
+
+				an := PolymorphismAnalyzer forModelNamed: '$system'. 
+				Snapshot new saveAs: '$stimdir/$system' thenQuit: true.
+				an methodStatistics.
+			] 
+			on: Error do: [:e| '$msefile - error', e printString]."
+
+		$app $im -evaluate "$command"
+	fi
+done
