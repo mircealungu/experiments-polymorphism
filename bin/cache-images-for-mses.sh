56fe986c76f49cc35d8491ec1095e3c8e4614541
Mircea <mircea.lungu@gmail.com>
2012-04-19 11:21:26 +0200

    cleaned up and made the test run

--- bin/cache-images-for-mses.sh
+++ bin/cache-images-for-mses.sh
@@ -2,18 +2,24 @@
 
 basedir=`cd .. && pwd`
 app=$basedir/bin/PolyExp.app/Contents/MacOS/visual 
-im=$basedir/bin/PolyExp.app/Contents/MacOS/resource.im
-stimdir=$basedir/bin/PolyExp.app/Contents/MacOS/visual 
+im=$basedir/bin/PolyExp.app/Contents/Resources/resource.im
+stimdir=$basedir/stimages
 
 for msefile in `ls $basedir/mse/*.mse`
 do
 	echo "building image for ..." $msefile
 	system=`basename $msefile .mse`
-	echo "will save new image in $stimdir/$system"
-	command="[|an| an := PolymorphismAnalyzer forFile: '$msefile'. Snapshot new saveAs: '$stimdir/$system.im' thenQuit: true.] on: Error do: [:e| '$msefile - error', e printString]."
-	#echo $command
+	if [ -e $stimdir/$system.im ]
+	then
+		echo $system is already cached
+	else
+		command="[|an| 
+				an := PolymorphismAnalyzer forFile: '$msefile'. 
+				Snapshot new saveAs: '$stimdir/$system' thenQuit: true.] 
+			on: Error do: [:e| '$msefile - error', e printString]."
 
-	#$app $im -evaluate $command
+		$app $im -evaluate "$command"
+	fi
 done
 
 
