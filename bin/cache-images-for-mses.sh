4174466f3c9382a5f59b438f7bbd7432d5407043
Mircea Lungu <lungu@iam.unibe.ch>
2012-04-20 16:04:10 +0200

    cache-images takes a parameter now

--- bin/cache-images-for-mses.sh
+++ bin/cache-images-for-mses.sh
@@ -3,9 +3,9 @@
 basedir=`cd .. && pwd`
 app=$basedir/bin/PolyExp.app/Contents/MacOS/visual 
 im=$basedir/bin/PolyExp.app/Contents/Resources/resource.im
-stimdir=$basedir/stimages
+stimdir=$basedir/stimages/$1
 
-for msefile in `ls $basedir/mse/*.mse`
+for msefile in `ls $basedir/mse/$1/*.mse`
 do
 	echo "building image for ..." $msefile
 	system=`basename $msefile .mse`
@@ -16,7 +16,7 @@ do
 		echo system is ... $system 
 		command="[|an| 
 			an := PolymorphismAnalyzer forFile: '$msefile'. 
-			Snapshot new saveAs: '$stimdir/$system' thenQuit: false.
+			Snapshot new saveAs: '$stimdir/$1/$system' thenQuit: false.
 			] 
 			on: Error do: [:e| '$msefile - error', e printString]."
 
